e9a4528750c130c0ae0bc4ba4e8132da2dd56cc3
mm <mmmdev2003@gmail.com>
2025-09-07 11:08:11 +0300

    Определил все модели и интерфейсы

--- internal/service/vacancy/service.py
+++ internal/service/vacancy/service.py
@@ -11,11 +11,15 @@ class VacancyService(interface.IVacancyService):
             self,
             tel: interface.ITelemetry,
             vacancy_repo: interface.IVacancyRepo,
+            interview_repo: interface.IInterviewRepo,
+            storage: interface.IStorage,
             vacancy_prompt_generator: interface.IVacancyPromptGenerator,
             llm_client: interface.ILLMClient,
     ):
         self.tracer = tel.tracer()
         self.logger = tel.logger()
         self.vacancy_repo = vacancy_repo
+        self.interview_repo = interview_repo
+        self.storage = storage
         self.vacancy_prompt_generator = vacancy_prompt_generator
         self.llm_client = llm_client
